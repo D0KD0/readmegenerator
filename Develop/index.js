8b33120dca4e22f2533898a32d5a8a84b3d9c3df
D0KD0 <113081917+D0KD0@users.noreply.github.com>
2022-11-13 21:37:43 -0800

    first draft

---
+++ Develop/index.js
@@ -0,0 +1,61 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name for github?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address for github?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is a name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project'
+    },
+    {
+        type: 'rawlist',
+        name: 'license',
+        choices: [
+            'MIT',
+            'GPLv3',
+            'AGPL',
+            'No license'
+        ]
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Any dependencies to be installed?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Something user should know?'
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Who take(s) the credit of this project?'
+    }
+
+];
+
+// TODO: Create a function to write README file
+inquirer
+    .prompt(questions)
+    .then((response) => fs.appendFile('README.md', writeToFile(response), (err) => err ? console.err(err) : console.log('ReadMe is successfully created!')));
+
+const writeToFile = (data) => generateMarkdown(data);
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,100 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  const licenseBadge = [
+    '![MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)', 
+    '![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)',
+    '![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)',
+    '![Mozilla License](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)'
+  ]
+
+  if(license === 'MIT') {
+    return licenseBadge[0];
+  } else if (license === 'GPLv3') {
+    return licenseBadge[1];
+  } else if (license === 'AGPL') {
+    return licenseBadge[2];
+  } else if (license === 'Mozilla') {
+    return licenseBadge[3];
+  } else {
+    return '';
+  }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  const licenseLink = [
+    'https://opensource.org/licenses/MIT',
+    'https://www.gnu.org/licenses/gpl-3.0',
+    'https://www.gnu.org/licenses/agpl-3.0',
+    'https://opensource.org/licenses/MPL-2.0'
+  ]
+
+  if (license === 'MIT') {
+    return licenseLink[0];
+  } else if (license === 'GPLv3') {
+    return licenseLink[1];
+  } else if (license === 'AGPL') {
+    return licenseLink[2];
+  } else if (license === 'Mozilla') {
+    return licenseLink[3];
+  } else {
+    return '';
+  }
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  const link = renderLicenseLink(license);
+
+  if (link === '') {
+    return `## License\n No license is applied for this git`
+  } else {
+    return `## License\n This git is licensed under the ${license}.\n ${link}`
+  }
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  const badge = renderLicenseBadge(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+
+  return `
+  # ${data.title}\n
+
+  ## Description
+
+  ${data.description}\n
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  \n
+
+  ## Installation
+
+  Needed dependencies for this project: ${data.installation} \n
+
+  ## Usage
+  ${data.usage} \n
+
+  ## Credits
+  ${data.credits} \n
+
+  ## License
+  ${badge}\n
+  This project is lisenced under the ${data.license} \n
+  Copyright (c) ${data.name}
+  
+  ## Contact
+  If there are any questions/concerns, pleaese email ${data.email}
+
+`;
+}
+
+module.exports = generateMarkdown;
